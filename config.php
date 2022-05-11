404c9357e52de569ce023dd3be493588ee0b10d8
MartinQuinn-Apleona <martin.quinn@apleona.com>
2022-05-11 11:31:22 +0100

    Config changes

--- config.php
+++ config.php
@@ -7,13 +7,16 @@
 	$myDB = "db_squirrel_dev"; */
 
   // DB Params
-  define("DB_HOST", "46.22.129.7");
-  define("DB_PASS", "7hJiy7*3");
+  //define("DB_HOST", "46.22.129.7");
+  //define("DB_PASS", "7hJiy7*3");
+
+  define("DB_HOST", "20.234.163.93");
+  define("DB_PASS", 'L8s"d:*=DK)J!CS^');
   
   //define("DB_USER", "usr_squirrel");
   //define("DB_NAME", "db_squirrel");  
   
-  define("DB_USER", "usr_squirrel_dev");
+  define("DB_USER", "apleona");
   define("DB_NAME", "db_squirrel_dev");
 
   //define("DB_HOST", "localhost");
@@ -21,7 +24,7 @@
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/Sqrl_V1.0');
+  define('URLROOT', 'http://optimise.apleonaserv.com/Sqrl');
   // Site Name
   define('SITENAME', 'Sqrl');
 ?>
